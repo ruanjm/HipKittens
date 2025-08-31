f750c349ab7b3c8f1da18b9f6a17fac05477c465
Simran Arora <emailsimran@gmail.com>
2025-08-30 19:52:23 -0500

    micro

--- kernels/TK/attn/gqa_backwards/micros/vector_ops/test_python.py
+++ kernels/TK/attn/gqa_backwards/micros/vector_ops/test_python.py
@@ -16,46 +16,27 @@ torch.manual_seed(0)
 b = 1
 h = 1
 n = 32
-d = 16
+d = 32
 
 # pytorch
-x_a = torch.randn((b, h, n, d), dtype=torch.bfloat16, device='cuda')
-x_b = torch.randn((b, h, n, d), dtype=torch.bfloat16, device='cuda')
-x_accum = torch.zeros((b, h, n, n), dtype=torch.float32, device='cuda')
+x = torch.randn((b, h, n, d), dtype=torch.float32, device='cuda')
+vec = torch.randn((b, h, d, 1), dtype=torch.float32, device='cuda')
 # y = x.sum(dim=-1, keepdim=True)
-y = torch.matmul(x_a, x_b.transpose(-2, -1)).float()
-# y = x_a
+y = x - vec
 
 # tk
-y_tk = torch.zeros_like(y).float()
-tk_kernel.dispatch_micro(x_a, x_b, y_tk, x_accum)
-
-print(y.shape, y_tk.shape)
+y_tk = torch.zeros_like(y)
+tk_kernel.dispatch_micro(x, vec, y_tk)
 
 # check
-diff = (y - y_tk).abs()
-max_diff = diff.max()
-print(y.shape,  x_a.shape, x_b.shape)
+diff = (y - y_tk).abs().max()
+print(y.shape, vec.shape, x.shape)
+print(f"diff: {diff}")
 
 
-# print(y[0, 0, 0:2, :8])
-print(diff[0, 0, 0:, :])
-num_diff = (diff > 1e-3).sum()
-print(f"num_diff: {num_diff} / {diff.numel()}")
-# print()
+print(y[0, 0, 0:2, :8])
+print(y_tk[0, 0, 0:2, :8])
+print()
 
-# print(x_b[0, 0, 0:, :].sum(dim=-1))
-# print(y_tk[0, 0, 0:1, :])
-# print(y[0, 0, 0:1, :])
-
-# print(x_b[0, 0, 0:16, :])
-# print(y_tk[0, 0, 0:16, :])
-# print(y[0, 0, 0:16, :])
-
-# print(y[0, 0, 8:10, :8])
-# print(y_tk[0, 0, 8:10, :8])
-print(f"diff: {max_diff}")
-
-
-
-
+print(y[0, 0, 3:6, :8])
+print(y_tk[0, 0, 3:6, :8])
--- kernels/TK/attn/gqa_backwards/micros/vector_ops/vector_micro.cpp
+++ kernels/TK/attn/gqa_backwards/micros/vector_ops/vector_micro.cpp
@@ -4,15 +4,16 @@ using namespace kittens;
 
 constexpr int b = 1;
 constexpr int h = 1;
-constexpr int n = 32;
-constexpr int d = 16;
+constexpr int n = 64;
+constexpr int d = 64;
 
 #define NUM_WARPS 1
 #define NUM_THREADS (kittens::WARP_THREADS * NUM_WARPS)
 
 struct micro_globals {
-    gl<bf16, -1, -1, -1, -1> in_a, in_b; 
-    gl<float, -1, -1, -1, -1> out, in_accum;
+    gl<float, -1, -1, -1, -1> tile; 
+    gl<float, -1, -1, -1, -1> vec;
+    gl<float, -1, -1, -1, -1> accum;
     dim3 grid()  { return dim3(1); } 
     dim3 block() { return dim3(NUM_THREADS); } 
     size_t dynamic_shared_memory() { return MAX_SHARED_MEMORY-2048; }
@@ -21,36 +22,18 @@ struct micro_globals {
 __global__ __launch_bounds__(NUM_THREADS, 1)
 void micro_tk(const micro_globals g) {
 
-    rt_bf<n, d, row_l> a_tile;
-    rt_bf<n, d, row_l> b_tile;
-    rt_fl<n, n, accum_col_l> tile_accum;
-    load(a_tile, g.in_a, {0, 0, 0, 0});
-    load(b_tile, g.in_b, {0, 0, 0, 0});
-    load(tile_accum, g.in_accum, {0, 0, 0, 0});
-    __builtin_amdgcn_sched_barrier(0);
+    rt_fl<n, d, accum_col_l> tile_accum; // mfma_32x32x16
+    load(tile_accum, g.tile, {0, 0, 0});
+    rt_fl<n, d, accum_col_l>::col_vec vec;
+    load(vec, g.vec, {0, 0, 0, 0});
+
+    sub_row(tile_accum, tile_accum, vec);
+    // row_sum(vec, tile_accum);
     __builtin_amdgcn_s_waitcnt(0);
     __builtin_amdgcn_sched_barrier(0);
     __builtin_amdgcn_s_barrier();
-    __builtin_amdgcn_sched_barrier(0);
-    __syncthreads();
-
-
-    mma_ABt(tile_accum, a_tile, b_tile, tile_accum);
-    __builtin_amdgcn_sched_barrier(0);
-    __builtin_amdgcn_s_waitcnt(0);
-    __builtin_amdgcn_sched_barrier(0);
-    __builtin_amdgcn_s_barrier();
-    __builtin_amdgcn_sched_barrier(0);
-    __syncthreads();
-
     
-    store(g.out, tile_accum, {0, 0, 0, 0});
-    __builtin_amdgcn_sched_barrier(0);
-    __builtin_amdgcn_s_waitcnt(0);
-    __builtin_amdgcn_sched_barrier(0);
-    __builtin_amdgcn_s_barrier();
-    __builtin_amdgcn_sched_barrier(0);
-    __syncthreads();
+    store(g.accum, tile_accum, {0, 0, 0, 0});
 }
 
 void dispatch_micro(micro_globals g) {
@@ -62,6 +45,6 @@ void dispatch_micro(micro_globals g) {
 
 PYBIND11_MODULE(tk_kernel, m) {
     m.doc() = "tk_kernel python module";
-    py::bind_function<dispatch_micro>(m, "dispatch_micro", &micro_globals::in_a, &micro_globals::in_b, &micro_globals::out, &micro_globals::in_accum);
+    py::bind_function<dispatch_micro>(m, "dispatch_micro", &micro_globals::tile, &micro_globals::vec, &micro_globals::accum);
 }
 
